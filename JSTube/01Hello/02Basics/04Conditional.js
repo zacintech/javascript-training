a800619070eba23258d03de4a7c094fd035e8752
zacintech <zacintech@gmail.com>
2022-12-27 22:48:50 +0530

    Conditional

---
+++ JSTube/01Hello/02Basics/04Conditional.js
@@ -0,0 +1,20 @@
+var temeperature
+
+//TODO Go to google and get the data.
+
+temperature = 39;
+
+if (temperature < 20) {
+    
+    console.log("its very cold outside");
+}
+
+if (temperature < 30) {
+    
+    console.log("its moderate outside");
+}
+
+else {
+    
+    console.log("Its really hot outside");
+}
