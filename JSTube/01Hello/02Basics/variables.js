719419fbc5fbf459cfe2871b3a924ba7cf061949
vigilantnetizen <72197793+vigilantnetizen@users.noreply.github.com>
2022-12-27 16:26:08 +0530

    javascript first module

---
+++ JSTube/01Hello/02Basics/variables.js
@@ -0,0 +1,92 @@
+var fullName = "Zac hossein";
+console.log(fullName);
+
+const uid = "abc123";
+
+
+
+function add(a,b) {
+    console.log(a + b);
+}
+
+
+
+
+function sub(a, b) {
+    console.log(a-b)
+}
+function mult(a, b) {
+    console.log(a*b)
+}
+function div(a, b) {
+    console.log(a/b)
+}
+
+
+function callAll() {
+    
+    let condition = "-";
+
+    sub(25, 10)
+    mult(5, 5)
+    div(21, 7)
+    if (condition == "+") {
+        add(10, 25);
+
+    }
+    
+
+}
+
+callAll();
+
+
+
+
+
+
+var fullName = "Zac Hossein";
+var email = "zacintech@gmail.com";
+var password = "123456";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+console.log(uid);
+console.log ("Full Name is:", fullName);
+console.log(email);
+
+console.log(`
+        With Unique ID: ${uid}
+        User is : ${fullName}
+        and his email is:${email}
+        and uses the password: ${password}
+
+`);
+
+
+var firstName = "Zac";
+var lastName = "Hossein"
+var email = "zacintech@gmail.com";
+var password = "123456";
+var country = "United Kingdom";
+var state = "gloucestershire";
+var courseCount = "0";
+var isLoggedInFromGoogle = "false";
+var isLoggedInFromFacebook = "true";
+
+
+console.log(`
+
+        users first name is  : ${firstName}
+        users last name is: ${lastName}
+        and his email is : ${email}
+        and his credentials are : ${password}
+        he is from : ${country}
+        his state is : ${state}
+        his course count is : ${courseCount}
+        is logged via : ${isLoggedInFromGoogle}
+        is logged using : ${isLoggedInFromFacebook}
+
+
+
+`);
