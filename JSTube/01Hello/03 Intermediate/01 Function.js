94e0378a9502047f67e484720771a5791307387a
zacintech <zacintech@gmail.com>
2022-12-28 11:55:38 +0530

    Basic Functions

---
+++ JSTube/01Hello/03 Intermediate/01 Function.js
@@ -0,0 +1,28 @@
+//function sayHello(name) {
+//    console.log("Hello there Zac");
+//    console.log("Hello there, ", name);
+//}
+
+//sayHello("Hitesh");
+//sayHello("Sammy");
+
+
+
+//function sayHello(name) {
+//    console.log("Hello there Zac");
+//    console.log(`Hello there, ${name}. How are you?`);
+//}
+
+//sayHello("Zac");
+//sayHello("Sammy");
+
+function namastey() {
+
+    return "Hello in India";
+}
+    
+var greetings = namastey();
+    
+
+console.log(greetings);
+console.log(namastey());
